652f5f65a77fe0ce394b5c419382bbd2a04dc8d7
npenilla <npenilla23@ufl.edu>
2019-01-19 19:16:51 -0500

    Increased web functions 7:15 pm

--- nutritionly-site/src/app/app.component.ts
+++ nutritionly-site/src/app/app.component.ts
@@ -2,9 +2,19 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
+  template: `
+    <!-- header -->
+    <app-header></app-header>
+
+    <!-- routes will be rendered here -->
+    <router-outlet></router-outlet>
+
+    <!-- footer -->
+    <app-footer></app-footer>
+  `,
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'nutritionly-site';
+  title = 'Nutrition.ly';
 }
